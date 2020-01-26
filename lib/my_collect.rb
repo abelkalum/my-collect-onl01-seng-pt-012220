9da2bbe221114bef7a52ac87f9914fe7459e0d16
Abel Kalum <chumbabel@gmail.com>
2020-01-26 21:16:36 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -4,7 +4,6 @@ def my_collect(languages)
   i = 0
   language.upcase = []
   while i < languages.length
-    my_collect(languages) do |language|
       languages.upcase << yield(language[i])
     i += 1
   end
