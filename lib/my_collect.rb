59c958da5c6b7a0887696bfe90a43c6dc092afbe
Abel Kalum <chumbabel@gmail.com>
2020-01-26 20:57:16 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def hello(array)
+  i = 0
+  collection = []
+  while i < array.length
+    collection << yield(array[i])
+    i += 1
+  end
+  collection
+end
 
+hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
 
