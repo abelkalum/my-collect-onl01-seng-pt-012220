b8bafac01978db3d64664a1ab545eaca388e3992
Abel Kalum <chumbabel@gmail.com>
2020-01-26 21:25:27 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -12,3 +12,12 @@ end
 
 students = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
+def my_collect(languages)
+  i = 0
+  my_collect = []
+    while i < languages.length
+      my_collect << yield(languages[i])
+    i += 1
+  end
+  my_collect
+end
