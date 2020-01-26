d15059a46c72a561f4fc4b60c8c5a37783659f5e
Abel Kalum <chumbabel@gmail.com>
2020-01-26 21:00:23 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
-def my_collect(array)
+def my_collect(languages)
   i = 0
   collection = []
-  while i < array.length
-    collection << yield(array[i])
+  while i < languages.length
+    collection << yield(languages[i])
     i += 1
   end
   collection
