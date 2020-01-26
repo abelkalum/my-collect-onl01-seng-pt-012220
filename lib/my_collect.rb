999c2d712c7996a47a68e24c4233ab2300fba8b5
Abel Kalum <chumbabel@gmail.com>
2020-01-26 21:20:18 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -2,8 +2,8 @@ languages = ['ruby', 'javascript', 'python', 'objective-c']
 
 def my_collect(languages)
   i = 0
-  languages.upcase = []
-  while i < languages.length
+  my_collect = []
+    while i < languages.length
       languages.upcase << yield(languages[i])
     i += 1
   end
